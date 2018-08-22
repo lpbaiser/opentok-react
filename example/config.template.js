c19a8fc77c3885578bfd3ec28b94d286541cdd3a
Leonardo Baiser <lpbaiser@gmail.com>
2018-08-22 16:09:17 -0300

    Inserindo index para require da lib

--- example/config.template.js
+++
@@ -1,6 +0,0 @@
-export default {
-  API_KEY: '123',
-  SESSION_ID: 'foo',
-  TOKEN: 'bar',
-  CHROME_EXTENSION_ID: 'baz'
-};
---
+++ index.js
@@ -0,0 +1,21 @@
+import OTSession from './src/OTSession';
+import OTPublisher from './src/OTPublisher';
+import OTStreams from './src/OTStreams';
+import OTSubscriber from './src/OTSubscriber';
+import createSession from './src/createSession';
+
+export default {
+  OTSession,
+  OTPublisher,
+  OTStreams,
+  OTSubscriber,
+  createSession,
+};
+
+export {
+  OTSession,
+  OTPublisher,
+  OTStreams,
+  OTSubscriber,
+  createSession,
+};
